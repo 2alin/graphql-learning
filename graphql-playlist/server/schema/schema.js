9c8aec19f1558514f667f0022aa6b166a1267405
2alin <adilsonsdev@gmail.com>
2019-06-02 22:43:01 -0500

    add dummy data

--- graphql-playlist/server/schema/schema.js
+++ graphql-playlist/server/schema/schema.js
@@ -2,6 +2,12 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+const books = [
+  { name: 'The Lord of The Rings', genre: 'Fantasy', id: '1' },
+  { name: 'Harry Potter', genre: 'Fantasy', id: '2' },
+  { name: 'How To Cook Rice', genre: 'Kitchen', id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
@@ -19,6 +25,7 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parent, args) {
         // code to get data from db / other source
+        return books.find(book => book.id === args.id);
       },
     },
   },
